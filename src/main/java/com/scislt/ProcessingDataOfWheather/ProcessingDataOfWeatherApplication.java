75819c92176229e1537bedbd8a3ec4ca1e69034f
Aliaksandr Hryharovich <algrigr@gmail.com>
2023-11-21 20:27:38 +0300

    Initial commit

---
+++ src/main/java/com/scislt/ProcessingDataOfWheather/ProcessingDataOfWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.scislt.ProcessingDataOfWheather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingDataOfWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingDataOfWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scislt/ProcessingDataOfWheather/ProcessingDataOfWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scislt.ProcessingDataOfWheather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingDataOfWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
